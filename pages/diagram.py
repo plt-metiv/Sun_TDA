41f4c1fccab2e1a0a21bb4815a15f1d0c7722894
plt-metiv <metiv.steam@gmail.com>
2024-07-02 06:31:54 +0330

    Update diagram.py

--- pages/diagram.py
+++ pages/diagram.py
@@ -85,7 +85,7 @@ def plot_persistence_diagrams( iloc_num, how_many = 3):
     data (array-like): Image data to plot.
     """
     for i in range(how_many):
-        data = get_image_matrix(iloc_num)
+        data = get_image_matrix(iloc_num+ i)
 
         data = (data - np.min(data)) /( np.max(data) - np.min(data)) 
 
@@ -136,53 +136,7 @@ def plot_persistence_diagrams( iloc_num, how_many = 3):
 
 
 
-        data = get_image_matrix(iloc_num+1)
-
-        data = (data - np.min(data)) /( np.max(data) - np.min(data)) 
-
-        data = crop_to_square(data,int( center_x - size/2 ),int(center_x + size/2 ) , int(center_y - size/2 ), int(center_y +  size/2 ))
-
-        
-        
-
-
-        # Step 6: Continue with Persistence Homology Analysis for sublevel set filtration
-        sublevel_cc = gd.CubicalComplex(top_dimensional_cells=data)
-        sublevel_cc.compute_persistence()
-
-        # Step 6: Continue with Persistence Homology Analysis for superlevel set filtration
-        superlevel_data = -data
-        superlevel_cc = gd.CubicalComplex(top_dimensional_cells=superlevel_data)
-        superlevel_cc.compute_persistence()
-
-
-
-
-
-        # Step 3: Plotting the image and the persistence diagram
-        fig, axs = plt.subplots(1, 2, figsize=(12, 6))
-
-        # Plot the image
-        axs[0].imshow(data, cmap='gray')
-        axs[0].set_title("Image")
-        axs[0].axis('off')
-
-        # Plot the persistence diagrams
-        axs[1].plot(sublevel_H0[:, 0], sublevel_H0[:, 1], '.k', markersize=5, alpha=0.3, label='Sublevel H0')
-        axs[1].plot(sublevel_H1[:, 0], sublevel_H1[:, 1], '.r', markersize=5, alpha=0.3, label='Sublevel H1')
-        axs[1].plot(-superlevel_H0[:, 0], -superlevel_H0[:, 1], 'xk', markersize=5, alpha=0.3, label='Superlevel H0')
-        axs[1].plot(-superlevel_H1[:, 0], -superlevel_H1[:, 1], 'xr', markersize=5, alpha=0.3, label='Superlevel H1')
-
-        axs[1].legend()
-        max_val = max(np.max(sublevel_H1), np.max(-superlevel_H1))
-        axs[1].plot(np.linspace(0, max_val, 20), np.linspace(0, max_val, 20), '--')
-
-        plt.tight_layout()
-        st.pyplot(fig, use_container_width=True) # instead of plt.show()
-
-
-
-how_many  = st.text_input("How many points to check after iloc num ", "3")
+how_many  = st.text_input("How many points to check after iloc num ", "2")
 if how_many:
     how_many = int(how_many)
 
